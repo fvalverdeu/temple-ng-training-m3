13882925b1969e31ee05d4bba629bb75520598da
Your Name <you@example.com>
2021-03-05 22:22:37 -0500

    feat: add apps and libraries

---
+++ libs/idstore-commons/src/lib/idstore-commons.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'is-idstore-commons',
+  template: `
+    <p>
+      idstore-commons works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class IdstoreCommonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ libs/idstore-commons/src/lib/idstore-commons.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { IdstoreCommonsComponent } from './idstore-commons.component';
+
+
+
+@NgModule({
+  declarations: [IdstoreCommonsComponent],
+  imports: [
+  ],
+  exports: [IdstoreCommonsComponent]
+})
+export class IdstoreCommonsModule { }
